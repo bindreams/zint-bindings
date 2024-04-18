c1eb8ea832389722bb4e073bca733c37008d5097
andreasxp <28830446+andreasxp@users.noreply.github.com>
2024-04-18 21:46:48 +0300

    Add tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,26 @@
-import pytest
+import logging
 
-import zint
+from zint import Symbol, Symbology
+
+
+def test_basic_usage():
+    s = Symbol()
+    s.symbology = Symbology.DATAMATRIX
+    s.encode(b"Hi mom!")
+    s.buffer(0)
+
+    assert s.bitmap is not None
+    assert s.bitmap.shape == (28, 28, 3)
+
+
+def test_warnings(caplog):
+    s = Symbol()
+    s.symbology = Symbology.AZTEC
+    s.option_1 = 10  # Incorrect option
+    s.encode(b"Hello")
+
+    logs = caplog.records
+    assert len(logs) == 1
+    assert logs[0].name == "zint"
+    assert logs[0].levelno == logging.WARNING
+    assert logs[0].msg == "Warning 503: Invalid error correction level - using default instead"
---
+++ tests/test_stub.py
@@ -0,0 +1,56 @@
+"""Test generated stub file.
+
+This script uses mypy's "stubtest" script to test the file. Some errors that mypy finds are allowed and this script
+filters them out.
+"""
+
+import re
+import subprocess as sp
+
+import pytest
+
+allowed_errors = [
+    r"zint\.\w+ is inconsistent, metaclass differs",
+    # Filtered out because the mentioned metaclass is a pybind11 implementation detail.
+    r"zint\.\w+\.__init__ is inconsistent, stub does not have \*args argument \"args\"",
+    # Filtered out because this error is raised for class which are not constructible from Python and don't have an
+    # __init__ in the stub file.
+    r"zint\.__all__ is not present at runtime",
+    # Filtered out because __all__ is a special variable and does not need to be present in the file.
+]
+
+for i, pattern in enumerate(allowed_errors):
+    allowed_errors[i] = re.compile(pattern)
+
+
+def test_stub():
+    sp.run(["stubtest", "--help"], check=True, stdout=sp.DEVNULL)  # Check that the executable works
+    errors = sp.run(["stubtest", "zint"], check=False, text=True, stderr=sp.STDOUT, stdout=sp.PIPE).stdout
+
+    fail = False
+    sections = errors.split("\n\n")
+    # Each section has the following format:
+    # ```
+    # error: zint.VectorRect.__init__ is inconsistent, stub does not have *args argument "args"
+    # Stub: in file [...]
+    # def (self: builtins.object)
+    # Runtime:
+    # def (self, /, *args, **kwargs)
+    # ```
+
+    for section in sections[:-1]:  # Last section of output is the summary
+        assert section.startswith("error: ")
+        error_text = section[len("error: ") :].split("\n")[0]
+        ok = False
+
+        for error in allowed_errors:
+            if re.fullmatch(error, error_text):
+                ok = True
+                break
+
+        if not ok:
+            fail = True
+            print(section, end="\n\n")
+
+    if fail:
+        pytest.fail("stubtest found errors which were not filtered (see output)")
